3d3008e0505c39c7cb5cbbbd15d751c77d1d943b
Kurill <fiavcukkirill7@gmail.com>
2026-02-18 21:35:32 +0500

    Домашние задание 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,9 +1,19 @@
 package main
 
+import "fmt"
 
-func  main(){
+func main() {
 	const USDvEUR = 0.8436
 	const USDvRUB = 76.62
-	const EURvRUB = USDvRUB/ USDvEUR
+	const EURvRUB = USDvRUB / USDvEUR
 
+}
+func outprint() float64 {
+	var outuser float64
+	fmt.Scan(&outuser)
+	return outuser
+
+}
+func valute(number float64, usd string, eur string){
+	
 }
